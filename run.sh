8d2a0baa3d82046581d777f2ae67f9852067a01a
John Chilton <jmchilton@gmail.com>
2013-02-19 13:42:01 -0600

    Add a little more documentation to new run.sh file.

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## Usage:
+## Start LWR server as daemon process:
+##   run.sh --daemon   
+## Stop LWR daemon process:
+##   run.sh --stop-daemon
+
 # Ensure working directory is lwr project. 
 PROJECT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $PROJECT_DIRECTORY
