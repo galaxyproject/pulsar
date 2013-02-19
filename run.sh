25250e81c20dd09aebad5e7b33a29305e7f4111b
John Chilton <jmchilton@gmail.com>
2013-02-19 13:39:30 -0600

    Fixup run.sh for non-virtualenv setups.

--- run.sh
+++ run.sh
@@ -4,5 +4,9 @@
 PROJECT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $PROJECT_DIRECTORY
 
-. .venv/bin/activate
+if [ -d .venv ]; 
+then
+    . .venv/bin/activate
+fi
+
 paster serve server.ini "$@"
