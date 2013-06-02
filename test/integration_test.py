e32779c45b2aeabadce40a1b0caba3dfc5d8ff96
John Chilton <jmchilton@gmail.com>
2013-06-02 08:12:29 -0500

    Extend new integration_test.py test to test private token security.

--- test/integration_test.py
+++ test/integration_test.py
@@ -15,8 +15,14 @@ class IntegrationTest(TestCase):
     def test_integration_curl(self):
         self.__run(private_token=None, transport="curl", cache=False, test_errors=False)
 
-    def __run(self, **kwds):
+    def test_integration_token(self):
+        self.__run(app_conf={"private_key": "testtoken"}, private_token="testtoken", transport="curl", cache=False, test_errors=False)
+
+    def test_integration_errors(self):
+        self.__run(app_conf={"private_key": "testtoken"}, private_token="testtoken", transport="curl", cache=False, test_errors=True)
+
+    def __run(self, app_conf={}, **kwds):
         from test_utils import test_server
-        with test_server() as server:
+        with test_server(app_conf=app_conf) as server:
             options = Bunch(url=server.application_url, **kwds)
             run(options)
