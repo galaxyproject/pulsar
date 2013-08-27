fb4c627c1cea3b44372f9a0f49ad12a76b97bbc4
John Chilton <jmchilton@gmail.com>
2013-08-27 12:41:55 -0500

    Fix bug where environment variable LWR_CACHE_THREADS was not being used, LWR_CACHE_TRANSFERS was being used instread. Add test cases to verify configuration of ClientManager is properly executed.

---
+++ test/client_manager_test.py
@@ -0,0 +1,31 @@
+from os import environ
+
+from lwr.lwr_client.manager import ClientManager
+
+
+def test_environment_variables_config():
+    environ['LWR_CACHE_TRANSFERS'] = '1'
+    client_manager = ClientManager()
+    assert __produces_caching_client(client_manager)
+
+    environ['LWR_CACHE_TRANSFERS'] = '0'
+    client_manager = ClientManager()
+    assert not __produces_caching_client(client_manager)
+
+    environ['LWR_CACHE_TRANSFERS'] = '1'
+    environ['LWR_CACHE_THREADS'] = '4'
+    client_manager = ClientManager()
+    client_manager.num_transfer_threads == 4
+
+
+def test_kwds_config():
+    client_manager = ClientManager(cache=True, transfer_threads=3)
+    assert __produces_caching_client(client_manager)
+    assert client_manager.num_transfer_threads == 3
+
+    client_manager = ClientManager(cache=False)
+    assert not __produces_caching_client(client_manager)
+
+
+def __produces_caching_client(client_manager):
+    return client_manager.client_class.__name__.find('Caching') > 0
