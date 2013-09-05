c8501b33de2fa0cff99e405734672686f133e055
John Chilton <jmchilton@gmail.com>
2013-09-04 19:27:30 -0500

    Start work on new lwr/managers/util package that will hopefully be for shared functionality between the LWR and Galaxy.

---
+++ lwr/managers/util/__init__.py
@@ -0,0 +1,5 @@
+"""
+This module and its submodules contains utilities for running external
+processes and interfacing with job managers. This module should contain
+functionality shared between Galaxy and the LWR.
+"""
