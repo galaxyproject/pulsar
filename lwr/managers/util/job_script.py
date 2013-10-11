de21e0d584dfb15158dccccc26e641cd0578d102
John Chilton <jmchilton@gmail.com>
2013-10-11 15:32:08 -0500

    Added ability to configure GALAXY_SLOTS environment variable to job_script.py.

--- lwr/managers/util/job_script.py
+++ lwr/managers/util/job_script.py
@@ -2,6 +2,8 @@ from string import Template
 
 DEFAULT_JOB_FILE_TEMPLATE = Template("""#!/bin/sh
 $headers
+GALAXY_SLOTS="$slots_expression"
+export GALAXY_SLOTS
 GALAXY_LIB="$galaxy_lib"
 if [ "$GALAXY_LIB" != "None" ]; then
     if [ -n "$PYTHONPATH" ]; then
@@ -21,7 +23,8 @@ REQUIRED_TEMPLATE_PARAMS = ['working_directory', 'command', 'exit_code_path']
 OPTIONAL_TEMPLATE_PARAMS = {
     'galaxy_lib': None,
     'headers': '',
-    'env_setup_commands': ''
+    'env_setup_commands': '',
+    'slots_expression': '1',
 }
 
 
@@ -44,6 +47,9 @@ def job_script(template=DEFAULT_JOB_FILE_TEMPLATE, **kwds):
     False
     >>> script = job_script(working_directory='wd', command='uptime', exit_code_path='ec', headers='#PBS -test')
     >>> script.startswith('#!/bin/sh\\n#PBS -test\\n')
+    True
+    >>> script = job_script(working_directory='wd', command='uptime', exit_code_path='ec', slots_expression='$SLURM_JOB_NUM_NODES')
+    >>> script.find('GALAXY_SLOTS="$SLURM_JOB_NUM_NODES"\\nexport GALAXY_SLOTS\\n') > 0
     True
     """
     if any([param not in kwds for param in REQUIRED_TEMPLATE_PARAMS]):
