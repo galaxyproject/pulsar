dfed5731084e55bc04f0084c446086fb0fab5ede
John Chilton <jmchilton@gmail.com>
2014-02-17 12:51:13 -0600

    Style fixes for flak8.

--- test/integration_test.py
+++ test/integration_test.py
@@ -83,7 +83,14 @@ class IntegrationTests(BaseIntegrationTest):
         self._run(private_token=None, default_file_action="remote_copy", local_setup=True, **self.default_kwargs)
 
     def test_message_queue(self):
-        self._run(app_conf=dict(message_queue_url="memory://test1"), private_token=None, default_file_action="remote_copy", local_setup=True, manager_url="memory://test1", **self.default_kwargs)
+        self._run(
+            app_conf=dict(message_queue_url="memory://test1"),
+            private_token=None,
+            default_file_action="remote_copy",
+            local_setup=True,
+            manager_url="memory://test1",
+            **self.default_kwargs
+        )
 
     def test_integration_copy(self):
         self._run(private_token=None, default_file_action="copy", **self.default_kwargs)
