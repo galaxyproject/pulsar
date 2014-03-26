96e9e1833438befa07a3de72bd1b241fe59b882b
John Chilton <jmchilton@gmail.com>
2014-03-26 09:04:11 -0500

    Don't place GALAXY_HOME on PYTHONPATH, tiny subset is copied in LWR now.
    It will be placed on path of tools.

--- run.sh
+++ run.sh
@@ -23,15 +23,6 @@ then
     . .venv/bin/activate
 fi
 
-# Set GALAXY_HOME to add Galaxy code base and dependencies to
-# PYTHONPATH. This is required for many stock Galaxy tools.
-if [ -n "$GALAXY_HOME" ]; 
-then
-    PYTHONPATH="$GALAXY_HOME/lib":$PYTHONPATH
-    export PYTHONPATH
-    echo "Added Galaxy libraries ($GALAXY_HOME/lib) to PYTHONPATH"
-fi
-
 # If TEST_GALAXY_LIBS is set, this script will attempt to verify
 # Galaxy is properly placed on the LWR's PYTHONPATH before starting
 # the server.
