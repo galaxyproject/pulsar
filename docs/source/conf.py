edefc7ed9be49b28112da7e0e00e2bbfabdc1aa3
John Chilton <jmchilton@gmail.com>
2014-06-18 13:52:11 -0500

    Update sphinx for LWR to Pulsar.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# LWR documentation build configuration file, created by
+# Pulsar documentation build configuration file, created by
 # sphinx-quickstart on Sun Dec 16 23:16:23 2012.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -42,8 +42,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'LWR'
-copyright = u'2012, John Chilton'
+project = u'Pulsar'
+copyright = u'201, John Chilton'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -185,8 +185,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'LWR.tex', u'LWR Documentation',
-   u'John Chilton', 'manual'),
+  ('index', 'Pulsar.tex', u'Pulsar Documentation',
+   u'The Galaxy Project', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -215,8 +215,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'lwr', u'LWR Documentation',
-     [u'John Chilton'], 1)
+    ('index', 'pulsar', u'Pulsar Documentation',
+     [u'Galaxy Project'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -229,8 +229,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'LWR', u'LWR Documentation',
-   u'John Chilton', 'LWR', 'One line description of project.',
+  ('index', 'Pulsar', u'Pulsar Documentation',
+   u'Galaxy Project', 'Pulsar', 'One line description of project.',
    'Miscellaneous'),
 ]
 
