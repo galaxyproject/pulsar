8cc99c64c95a7b15b077aec0183c8dec4714d5a4
John Chilton <jmchilton@gmail.com>
2014-06-18 14:29:47 -0500

    Rename LwrController.

--- pulsar/web/routes.py
+++ pulsar/web/routes.py
@@ -20,10 +20,10 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class LwrController(Controller):
+class PulsarController(Controller):
 
     def __init__(self, **kwargs):
-        super(LwrController, self).__init__(**kwargs)
+        super(PulsarController, self).__init__(**kwargs)
 
     def _check_access(self, req, environ, start_response):
         if req.app.private_key:
@@ -42,7 +42,7 @@ class LwrController(Controller):
         return app_args
 
 
-@LwrController(path="/jobs", method="POST", response_type='json')
+@PulsarController(path="/jobs", method="POST", response_type='json')
 def setup(manager, job_id, tool_id=None, tool_version=None):
     return __setup(manager, job_id, tool_id=tool_id, tool_version=tool_version)
 
@@ -53,12 +53,12 @@ def __setup(manager, job_id, tool_id, tool_version):
     return response
 
 
-@LwrController(path="/jobs/{job_id}", method="DELETE")
+@PulsarController(path="/jobs/{job_id}", method="DELETE")
 def clean(manager, job_id):
     manager.clean(job_id)
 
 
-@LwrController(path="/jobs/{job_id}/submit", method="POST")
+@PulsarController(path="/jobs/{job_id}/submit", method="POST")
 def submit(manager, job_id, command_line, params='{}', dependencies_description='null', setup_params='{}', remote_staging='[]', env='[]'):
     submit_params = loads(params)
     setup_params = loads(setup_params)
@@ -77,25 +77,25 @@ def submit(manager, job_id, command_line, params='{}', dependencies_description=
     submit_job(manager, submit_config)
 
 
-@LwrController(path="/jobs/{job_id}/status", response_type='json')
+@PulsarController(path="/jobs/{job_id}/status", response_type='json')
 def status(manager, job_id):
     status = manager.get_status(job_id)
     return full_status(manager, status, job_id)
 
 
-@LwrController(path="/jobs/{job_id}/cancel", method="PUT")
+@PulsarController(path="/jobs/{job_id}/cancel", method="PUT")
 def cancel(manager, job_id):
     manager.kill(job_id)
 
 
-@LwrController(path="/jobs/{job_id}/files", method="POST", response_type='json')
+@PulsarController(path="/jobs/{job_id}/files", method="POST", response_type='json')
 def upload_file(manager, type, file_cache, job_id, name, body, cache_token=None):
     # Input type should be one of input, config, workdir, tool, or unstructured (see action_mapper.path_type)
     path = manager.job_directory(job_id).calculate_path(name, type)
     return _handle_upload(file_cache, path, body, cache_token=cache_token)
 
 
-@LwrController(path="/jobs/{job_id}/files/path", method="GET", response_type='json')
+@PulsarController(path="/jobs/{job_id}/files/path", method="GET", response_type='json')
 def path(manager, type, job_id, name):
     if type in [path_type.OUTPUT, path_type.OUTPUT_WORKDIR]:
         path = _output_path(manager, job_id, name, type)
@@ -104,7 +104,7 @@ def path(manager, type, job_id, name):
     return {'path': path}
 
 
-@LwrController(path="/jobs/{job_id}/files", method="GET", response_type='file')
+@PulsarController(path="/jobs/{job_id}/files", method="GET", response_type='file')
 def download_output(manager, job_id, name, type=path_type.OUTPUT):
     return _output_path(manager, job_id, name, type)
 
@@ -127,14 +127,14 @@ def _output_path(manager, job_id, name, output_type):
     return path
 
 
-@LwrController(path="/cache/status", method="GET", response_type='json')
+@PulsarController(path="/cache/status", method="GET", response_type='json')
 def file_available(file_cache, ip, path):
     """ Returns {token: <token>, ready: <bool>}
     """
     return file_cache.file_available(ip, path)
 
 
-@LwrController(path="/cache", method="PUT", response_type='json')
+@PulsarController(path="/cache", method="PUT", response_type='json')
 def cache_required(file_cache, ip, path):
     """ Returns bool indicating whether this client should
     execute cache_insert. Either way client should be follow up
@@ -143,7 +143,7 @@ def cache_required(file_cache, ip, path):
     return file_cache.cache_required(ip, path)
 
 
-@LwrController(path="/cache", method="POST", response_type='json')
+@PulsarController(path="/cache", method="POST", response_type='json')
 def cache_insert(file_cache, ip, path, body):
     temp_path = copy_to_temp(body)
     file_cache.cache_file(temp_path, ip, path)
@@ -151,13 +151,13 @@ def cache_insert(file_cache, ip, path, body):
 
 # TODO: coerce booleans and None values into correct types - simplejson may
 # do this already but need to check.
-@LwrController(path="/objects/{object_id}/exists", response_type='json')
+@PulsarController(path="/objects/{object_id}/exists", response_type='json')
 def object_store_exists(object_store, object_id, base_dir=None, dir_only=False, extra_dir=None, extra_dir_at_root=False, alt_name=None):
     obj = LwrDataset(object_id)
     return object_store.exists(obj, base_dir=base_dir, dir_only=dir_only, extra_dir=extra_dir, extra_dir_at_root=extra_dir_at_root, alt_name=alt_name)
 
 
-@LwrController(path="/objects/{object_id}/file_ready", response_type='json')
+@PulsarController(path="/objects/{object_id}/file_ready", response_type='json')
 def object_store_file_ready(object_store, object_id, base_dir=None, dir_only=False, extra_dir=None, extra_dir_at_root=False, alt_name=None):
     obj = LwrDataset(object_id)
     return object_store.file_ready(obj, base_dir=base_dir, dir_only=dir_only,
@@ -165,31 +165,31 @@ def object_store_file_ready(object_store, object_id, base_dir=None, dir_only=Fal
                                    alt_name=alt_name)
 
 
-@LwrController(path="/objects/{object_id}", method="POST", response_type='json')
+@PulsarController(path="/objects/{object_id}", method="POST", response_type='json')
 def object_store_create(object_store, object_id, base_dir=None, dir_only=False, extra_dir=None, extra_dir_at_root=False, alt_name=None):
     obj = LwrDataset(object_id)
     return object_store.create(obj, base_dir=base_dir, dir_only=dir_only, extra_dir=extra_dir, extra_dir_at_root=extra_dir_at_root, alt_name=alt_name)
 
 
-@LwrController(path="/objects/{object_id}/empty", response_type='json')
+@PulsarController(path="/objects/{object_id}/empty", response_type='json')
 def object_store_empty(object_store, object_id, base_dir=None, extra_dir=None, extra_dir_at_root=False, alt_name=None):
     obj = LwrDataset(object_id)
     return object_store.empty(obj, base_dir=base_dir, extra_dir=extra_dir, extra_dir_at_root=extra_dir_at_root, alt_name=alt_name)
 
 
-@LwrController(path="/objects/{object_id}/size", response_type='json')
+@PulsarController(path="/objects/{object_id}/size", response_type='json')
 def object_store_size(object_store, object_id, extra_dir=None, extra_dir_at_root=False, alt_name=None):
     obj = LwrDataset(object_id)
     return object_store.size(obj, extra_dir=extra_dir, extra_dir_at_root=extra_dir_at_root, alt_name=alt_name)
 
 
-@LwrController(path="/objects/{object_id}", method="DELETE", response_type='json')
+@PulsarController(path="/objects/{object_id}", method="DELETE", response_type='json')
 def object_store_delete(object_store, object_id, entire_dir=False, base_dir=None, extra_dir=None, extra_dir_at_root=False, alt_name=None):
     obj = LwrDataset(object_id)
     return object_store.delete(obj, entire_dir=False, base_dir=None, extra_dir=extra_dir, extra_dir_at_root=extra_dir_at_root, alt_name=alt_name)
 
 
-@LwrController(path="/objects/{object_id}", method="GET", response_type='json')
+@PulsarController(path="/objects/{object_id}", method="GET", response_type='json')
 def object_store_get_data(object_store, object_id, start=0, count=-1, base_dir=None, extra_dir=None, extra_dir_at_root=False, alt_name=None):
     obj = LwrDataset(object_id)
     return object_store.get_data(obj, start=int(start), count=int(count), entire_dir=False,
@@ -197,14 +197,14 @@ def object_store_get_data(object_store, object_id, start=0, count=-1, base_dir=N
                                  alt_name=alt_name)
 
 
-@LwrController(path="/objects/{object_id}/filename", response_type='json')
+@PulsarController(path="/objects/{object_id}/filename", response_type='json')
 def object_store_get_filename(object_store, object_id, base_dir=None, dir_only=False, extra_dir=None, extra_dir_at_root=False, alt_name=None):
     obj = LwrDataset(object_id)
     return object_store.get_filename(obj, base_dir=base_dir, dir_only=dir_only, extra_dir=extra_dir,
                                      extra_dir_at_root=extra_dir_at_root, alt_name=alt_name)
 
 
-@LwrController(path="/objects/{object_id}", method="PUT", response_type='json')
+@PulsarController(path="/objects/{object_id}", method="PUT", response_type='json')
 def object_store_update_from_file(object_store, object_id, base_dir=None, extra_dir=None, extra_dir_at_root=False,
                                   alt_name=None, file_name=None, create=False):
     obj = LwrDataset(object_id)
@@ -212,7 +212,7 @@ def object_store_update_from_file(object_store, object_id, base_dir=None, extra_
                                          alt_name=alt_name, file_name=file_name, create=create)
 
 
-@LwrController(path="/object_store_usage_percent", response_type='json')
+@PulsarController(path="/object_store_usage_percent", response_type='json')
 def object_store_get_store_usage_percent(object_store):
     return object_store.get_store_usage_percent()
 
