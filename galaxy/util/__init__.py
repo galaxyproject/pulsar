ffff47449bc1a8c8cc39bcecb17766ddc831e76f
John Chilton <jmchilton@gmail.com>
2014-10-05 22:48:42 -0400

    Python 3 fixes from work on planemo.

--- galaxy/util/__init__.py
+++ galaxy/util/__init__.py
@@ -78,14 +78,14 @@ def umask_fix_perms(path, umask, unmasked_perms, gid=None):
     perms = unmasked_perms & ~umask
     try:
         st = os.stat(path)
-    except OSError, e:
+    except OSError as e:
         log.exception('Unable to set permissions or group on %s' % path)
         return
     # fix modes
     if stat.S_IMODE(st.st_mode) != perms:
         try:
             os.chmod(path, perms)
-        except Exception, e:
+        except Exception as e:
             log.warning('Unable to honor umask (%s) for %s, tried to set: %s but mode remains %s, error was: %s' % (oct(umask),
                                                                                                                     path,
                                                                                                                     oct(perms),
@@ -95,7 +95,7 @@ def umask_fix_perms(path, umask, unmasked_perms, gid=None):
     if gid is not None and st.st_gid != gid:
         try:
             os.chown(path, -1, gid)
-        except Exception, e:
+        except Exception as e:
             try:
                 desired_group = grp.getgrgid(gid)
                 current_group = grp.getgrgid(st.st_gid)
@@ -144,7 +144,7 @@ def asbool(obj):
 def force_symlink(source, link_name):
     try:
         os.symlink(source, link_name)
-    except OSError, e:
+    except OSError as e:
         if e.errno == errno.EEXIST:
             os.remove(link_name)
             os.symlink(source, link_name)
