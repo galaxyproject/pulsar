e01c8db0224edd25dd0570e42fcb02ba97be490c
John Chilton <jmchilton@gmail.com>
2015-02-27 01:01:47 -0500

    Project organization improvement.
     - Add a version.
     - Add a HISTORY.rst file.
     - Outline a setup.py.
     - Update .gitignore for build artifacts.

---
+++ HISTORY.rst
@@ -0,0 +1,22 @@
+.. :changelog:
+
+History
+-------
+
+------------------------
+0.3.0.dev0
+------------------------
+
+ * Changed named to Pulsar, moved to Github.
+ * New RESTful web services interface.
+
+---------------------
+0.2.0
+---------------------
+
+ * Last version of LWR - found on BitBucket at https://bitbucket.org/jmchilton/lwr.
+ * Still supported in Galaxy as of 15.03 the release.
+
+---------------------
+0.1.0
+---------------------
---
+++ setup.py
@@ -0,0 +1,96 @@
+import re
+import ast
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    'six',
+    'webob',
+    'psutil',
+    'paste',
+    'PasteScript',
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('pulsar/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='pulsar',
+    version=version,
+    description='Distributed job execution application built for Galaxy (http://galaxyproject.org/).',
+    long_description=readme + '\n\n' + history,
+    author='Galaxy Project',
+    author_email='jmchilton@gmail.com',
+    url='https://github.com/galaxyproject/pulsar',
+    packages=[
+        'pulsar',
+        'pulsar.cache',
+        'pulsar.client',
+        'pulsar.client.staging',
+        'pulsar.client.transport',
+        'pulsar.managers',
+        'pulsar.managers.base',
+        'pulsar.managers.staging',
+        'pulsar.managers.util',
+        'pulsar.managers.util.cli',
+        'pulsar.managers.util.cli.job',
+        'pulsar.managers.util.cli.shell',
+        'pulsar.managers.util.condor',
+        'pulsar.managers.util.drmaa',
+        'pulsar.managers.util.job_script',
+        'pulsar.mesos',
+        'pulsar.messaging',
+        'pulsar.scripts',
+        'pulsar.tools',
+        'pulsar.web',
+        'galaxy',
+        'galaxy.jobs',
+        'galaxy.jobs.metrics',
+        'galaxy.jobs.metrics.collectl',
+        'galaxy.jobs.metrics.instrumenters',
+        'galaxy.objectstore',
+        'galaxy.tools',
+        'galaxy.tools.linters',
+        'galaxy.tools.deps',
+        'galaxy.tools.deps.resolvers',
+        'galaxy.util',
+    ],
+    entry_points='''
+        [console_scripts]
+        pulsar=pulsar.main
+    ''',
+    package_dir={'pulsar': 'pulsar',
+                 'galaxy': 'galaxy'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache License 2.0",
+    zip_safe=False,
+    keywords='pulsar',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='test',
+    tests_require=test_requirements
+)
