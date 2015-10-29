galaxyproject/pulsar

commit 22070bc275fefac68846b971c63d6f9c6c5e50d8

Rename lwr.lwr_client to lwr.client.

update_galaxy_client.sh
@@ -33,13 +33,13 @@ done
 shift "$((OPTIND-1))" # Shift off the options and optional --.
 LWR_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GALAXY_DIRECTORY=$1
-GALAXY_RUNNERS_DIRECTORY=$GALAXY_DIRECTORY/lib/galaxy/jobs/runners/
+GALAXY_PULSAR_DIRECTORY=$GALAXY_DIRECTORY/lib/pulsar/
 
 if [ "$invert" -ne "1" ];
 then
-	rm -rf $GALAXY_RUNNERS_DIRECTORY/lwr_client
-	cp -r $LWR_DIRECTORY/lwr/lwr_client $GALAXY_RUNNERS_DIRECTORY
+	rm -rf $GALAXY_RUNNERS_DIRECTORY/client
+	cp -r $LWR_DIRECTORY/lwr/client $GALAXY_PULSAR_DIRECTORY
 else
-	rm -rf $LWR_DIRECTORY/lwr/lwr_client
-	cp -r $GALAXY_RUNNERS_DIRECTORY/lwr_client $LWR_DIRECTORY/lwr
+	rm -rf $LWR_DIRECTORY/lwr/client
+	cp -r $GALAXY_PULSAR_DIRECTORY/client $LWR_DIRECTORY/lwr
 fi

galaxy/objectstore/lwr.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import  # Need to import lwr_client absolutely.
 from ..objectstore import ObjectStore
 try:
-    from galaxy.jobs.runners.lwr_client.manager import ObjectStoreClientManager
+    from pulsar.client.manager import ObjectStoreClientManager
 except ImportError:
-    from lwr.lwr_client.manager import ObjectStoreClientManager
+    from lwr.client.manager import ObjectStoreClientManager
 
 
 class LwrObjectStore(ObjectStore):

lwr/client/__init__.py
@@ -1,6 +1,6 @@
 """
-lwr_client
-==========
+client
+======
 
 This module contains logic for interfacing with an external LWR server.
 

commit 140a06913162cc90d9c2936ce8f41b44f4376325

Fix bug introduced in 22070bc275fefac68846b971c63d6f9c6c5e50d8 .

update_galaxy_client.sh
@@ -3,7 +3,7 @@
 usage() {
 cat << EOF
 Usage: ${0##*/} [-i] /path/to/galaxy...
-Sync Pulsar client to with copy in specified Galaxy directory (or vice versa if -i).
+Sync Pulsar client with copy in specified Galaxy directory (or vice versa if -i).
 
 EOF
 }
@@ -37,7 +37,7 @@ GALAXY_PULSAR_DIRECTORY=$GALAXY_DIRECTORY/lib/pulsar/
 
 if [ "$invert" -ne "1" ];
 then
-	rm -rf $GALAXY_RUNNERS_DIRECTORY/client
+	rm -rf $GALAXY_PULSAR_DIRECTORY/client
 	cp -r $PULSAR_DIRECTORY/pulsar/client $GALAXY_PULSAR_DIRECTORY
 else
 	rm -rf $PULSAR_DIRECTORY/pulsar/client