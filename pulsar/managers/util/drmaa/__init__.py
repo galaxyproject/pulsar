8f7c04ac63d7df6135521b135b2f09d892962f6b
Nate Coraor <nate@bx.psu.edu>
2016-04-06 15:35:02 -0400

    Use a global DRMAA session (only one active DRMAA session is possible per process) so multiple runner plugins can be active in the same runner (they all have to target the same DRMAA library, however). Also add locking to calls that modify the number of active jobs, as per the Python-drmaa documentation.

--- pulsar/managers/util/drmaa/__init__.py
+++ pulsar/managers/util/drmaa/__init__.py
@@ -1,3 +1,6 @@
+import threading
+import logging
+
 try:
     from drmaa import Session, JobControlAction
 except OSError as e:
@@ -9,6 +12,8 @@ except ImportError as e:
     Session = None
 
 NO_DRMAA_MESSAGE = "Attempt to use DRMAA, but DRMAA Python library cannot be loaded. "
+
+log = logging.getLogger(__name__)
 
 
 class DrmaaSessionFactory(object):
@@ -22,39 +27,46 @@ class DrmaaSessionFactory(object):
         session_constructor = self.session_constructor
         if session_constructor is None:
             raise Exception(NO_DRMAA_MESSAGE + LOAD_ERROR_MESSAGE)
-        return DrmaaSession(session_constructor(), **kwds)
+        return DrmaaSession(session_constructor, **kwds)
 
 
 class DrmaaSession(object):
     """
     Abstraction around `drmaa` module `Session` objects.
     """
+    session_lock = threading.Semaphore(1)
+    session = None
 
-    def __init__(self, session, **kwds):
-        self.session = session
-        session.initialize()
+    def __init__(self, session_constructor, **kwds):
+        with DrmaaSession.session_lock:
+            if DrmaaSession.session is None:
+                log.debug("Initializing DRMAA session from thread %s", threading.current_thread().name)
+                DrmaaSession.session = session_constructor()
+                DrmaaSession.session.initialize()
 
     def run_job(self, **kwds):
         """
         Create a DRMAA job template, populate with specified properties,
         run the job, and return the external_job_id.
         """
-        template = self.session.createJobTemplate()
+        template = DrmaaSession.session.createJobTemplate()
         try:
             for key in kwds:
                 setattr(template, key, kwds[key])
-            return self.session.runJob(template)
+            with DrmaaSession.session_lock:
+                return DrmaaSession.session.runJob(template)
         finally:
-            self.session.deleteJobTemplate(template)
+            DrmaaSession.session.deleteJobTemplate(template)
 
     def kill(self, external_job_id):
-        return self.session.control(str(external_job_id), JobControlAction.TERMINATE)
+        with DrmaaSession.session_lock:
+            return DrmaaSession.session.control(str(external_job_id), JobControlAction.TERMINATE)
 
     def job_status(self, external_job_id):
-        return self.session.jobStatus(str(external_job_id))
+        return DrmaaSession.session.jobStatus(str(external_job_id))
 
     def close(self):
-        return self.session.exit()
+        return DrmaaSession.session.exit()
 
 
 __all__ = ['DrmaaSessionFactory']
