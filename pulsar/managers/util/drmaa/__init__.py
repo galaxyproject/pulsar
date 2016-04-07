76080daccc3521cb369d2efb56fa4aecb154b657
John Chilton <jmchilton@gmail.com>
2016-04-07 10:12:58 -0400

    Revise handling of global session in drmaa factory.

--- pulsar/managers/util/drmaa/__init__.py
+++ pulsar/managers/util/drmaa/__init__.py
@@ -1,5 +1,6 @@
-import threading
+import contextlib
 import logging
+import threading
 
 try:
     from drmaa import Session, JobControlAction
@@ -35,14 +36,24 @@ class DrmaaSession(object):
     Abstraction around `drmaa` module `Session` objects.
     """
     session_lock = threading.Lock()
+    session_count = 0
     session = None
 
     def __init__(self, session_constructor, **kwds):
-        with DrmaaSession.session_lock:
+        with self._session_lock():
             if DrmaaSession.session is None:
+                if DrmaaSession.session_count != 0:
+                    log.warn("DrmaaSession.session is None but session_count is non-zero - logic error occurred.")
                 log.debug("Initializing DRMAA session from thread %s", threading.current_thread().name)
                 DrmaaSession.session = session_constructor()
                 DrmaaSession.session.initialize()
+            DrmaaSession.session_count += 1
+
+
+    @contextlib.contextmanager
+    def _session_lock(self):
+        with DrmaaSession.session_lock:
+            yield
 
     def run_job(self, **kwds):
         """
@@ -66,7 +77,19 @@ class DrmaaSession(object):
         return DrmaaSession.session.jobStatus(str(external_job_id))
 
     def close(self):
-        return DrmaaSession.session.exit()
+        with self._session_lock():
+            if DrmaaSession.session_count == 0:
+                log.warn("close() called with zero active session counted - logic error.")
+                return
+
+            DrmaaSession.session_count -= 1
+            if DrmaaSession.session_count == 0:
+                if DrmaaSession.session is None:
+                    log.warn("close() called with a non-zero session count but no session is defined.")
+                    return
+
+                DrmaaSession.session.exit()
+                DrmaaSession.session = None
 
 
 __all__ = ['DrmaaSessionFactory']
