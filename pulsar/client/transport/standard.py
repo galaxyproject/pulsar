f14c4dda7176459a2720db08f19b9e8d12ab9247
mvdbeek <m.vandenbeek@gmail.com>
2020-02-28 18:49:13 +0100

    Use requests

--- pulsar/client/transport/standard.py
+++ pulsar/client/transport/standard.py
@@ -4,65 +4,30 @@ Pulsar HTTP Client layer based on Python Standard Library (urllib2)
 
 from __future__ import with_statement
 
-import mmap
-import socket
+import requests
 
-from os.path import getsize
-try:
-    from urllib2 import urlopen, URLError
-except ImportError:
-    from urllib.request import urlopen
-    from urllib.error import URLError
-try:
-    from urllib2 import Request
-except ImportError:
-    from urllib.request import Request
 
 from ..exceptions import PulsarClientTransportError
 
 
 class Urllib2Transport(object):
 
-    def __init__(self, timeout=None, **kwrgs):
+    def __init__(self, timeout=None, **kwargs):
         self.timeout = timeout
 
-    def _url_open(self, request, data):
-        return urlopen(request, data, self.timeout)
-
     def execute(self, url, method=None, data=None, input_path=None, output_path=None):
-        request = self.__request(url, data, method)
-        input = None
+        exec_method = getattr(requests, method.lower() if method else 'get')
+        kwargs = {'url': url, 'data': data}
+        if input_path:
+            kwargs['files'] = {'file': open(input_path, 'rb')}
         try:
-            if input_path:
-                size = getsize(input_path)
-                if size:
-                    input = open(input_path, 'rb')
-                    data = mmap.mmap(input.fileno(), 0, access=mmap.ACCESS_READ)
-                else:
-                    data = b""
-                request.add_header('Content-Length', str(size))
-            try:
-                response = self._url_open(request, data)
-            except socket.timeout:
-                raise PulsarClientTransportError(code=PulsarClientTransportError.TIMEOUT)
-            except URLError as exc:
-                raise PulsarClientTransportError(transport_message=exc.reason)
-        finally:
-            if input:
-                input.close()
+            response = exec_method(**kwargs)
+        except Exception as e:
+            raise PulsarClientTransportError(transport_message=str(e))
         if output_path:
             with open(output_path, 'wb') as output:
-                while True:
-                    buffer = response.read(1024)
-                    if not buffer:
-                        break
-                    output.write(buffer)
-            return response
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:  # filter out keep-alive new chunks
+                        output.write(chunk)
         else:
-            return response.read()
-
-    def __request(self, url, data, method):
-        request = Request(url=url, data=data)
-        if method:
-            request.get_method = lambda: method
-        return request
+            return response.content
