fcd325c8abf0fdab2cf38d1a914b20970b96c182
mvdbeek <m.vandenbeek@gmail.com>
2020-03-01 12:34:00 +0100

    Rename requests to avoid collision

--- pulsar/client/transport/__init__.py
+++ pulsar/client/transport/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from .curl import curl_available
 from .curl import PycurlTransport
-from .requests import requests_multipart_post_available
+from ._requests import requests_multipart_post_available
 from .ssh import rsync_get_file, scp_get_file
 from .ssh import rsync_post_file, scp_post_file
 from .standard import Urllib2Transport
