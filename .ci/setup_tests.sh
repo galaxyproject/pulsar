365c0923270a0c497cfe79d1ae918864f6e230ae
Nate Coraor <nate@bx.psu.edu>
2021-02-02 13:38:52 -0500

    Github workflow fixes

--- .ci/setup_tests.sh
+++ .ci/setup_tests.sh
@@ -5,7 +5,7 @@ set -v
 
 sudo add-apt-repository ppa:natefoo/slurm-drmaa -y
 sudo apt update
-sudo apt install -y libxml2-dev libxslt1-dev libcurl3 python-pycurl openssh-server
+sudo apt install -y libxml2-dev libxslt1-dev libcurl4-openssl-dev python-pycurl openssh-server
 #pip install -r requirements$REQUIREMENTS_SUFFIX.txt --use-mirrors || true
 #pip install -r dev-requirements.txt --use-mirrors || true
 pip install coveralls  # Required fro coveralls reporting.
@@ -18,6 +18,7 @@ yes | sudo /usr/sbin/create-munge-key
 sudo service munge start
 sudo python scripts/configure_test_slurm.py
 echo "export DRMAA_LIBRARY_PATH=/usr/lib/slurm-drmaa/lib/libdrmaa.so" >> local_env.sh
-echo ". $VIRTUAL_ENV/bin/activate" >> local_env.sh
-sudo useradd --home-dir /home/u1 --shell /bin/bash --create-home --comment TEST,,, --groups travis u1  ## Create user for run-as-user test.
+#echo ". $VIRTUAL_ENV/bin/activate" >> local_env.sh
+echo "umask 002" >> local_env.sh
+sudo useradd --home-dir /home/u1 --shell /bin/bash --create-home --comment TEST,,, --groups $(id -gn) u1  ## Create user for run-as-user test.
 mkdir -p ~/.ssh && cp test_data/testkey.pub ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys
