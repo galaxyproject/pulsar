0bdda0f16e57cd989dcc7342ab78f9bea15d60a8
José Manuel Domínguez <dominguj@informatik.uni-freiburg.de>
2025-01-20 16:23:20 +0100

    Refactor tests in test_cli_submit.py
    Restore original test behavior but adapting the code to make room for a new test `CliCommandLineAppConfigTestCase`.
    
    For now, remove the draft code for `CliCommandLineAppConfigTestCase`.

--- test/test_cli_submit.py
+++ test/test_cli_submit.py
@@ -1,4 +1,9 @@
 import os
+from abc import (
+    ABC,
+    abstractmethod,
+)
+
 import yaml
 
 from .test_utils import (
@@ -6,7 +11,6 @@ from .test_utils import (
     files_server,
     integration_test,
     skip_unless_module,
-    temp_directory_persist,
 )
 
 from pulsar.client import ClientOutputs, ClientInput
@@ -14,78 +18,101 @@ from pulsar.client.util import to_base64_json
 from pulsar.scripts import submit
 
 
-class BaseCliTestCase(TempDirectoryTestCase):
-
-    def setup_action_mapper(self, files_endpoint):
-        return {
-            "default_action": "remote_transfer",
-            "files_endpoint": files_endpoint,
-        }
+class BaseCliTestCase(ABC, TempDirectoryTestCase):
 
     def run_and_check_submission(self):
+        # prepare job input directory
+        input_directory = os.path.join(self.temp_directory, "input_files")
+        os.makedirs(input_directory, exist_ok=False)
+        self.setup_input_directory(input_directory)
+
+        # prepare job output directory
+        output_directory = os.path.join(self.temp_directory, "output_files")
+        output_file_name = "dataset_1211231231231231231.dat"
+        output_file_path = os.path.join(output_directory, output_file_name)
+        os.makedirs(output_directory, exist_ok=False)
+
+        # prepare Galaxy working directory
+        galaxy_working_directory = os.path.join(self.temp_directory, "galaxy_working")
+        os.makedirs(galaxy_working_directory, exist_ok=False)
+
         job_id = "0"
-        galaxy_working = temp_directory_persist()
-        input_name = "dataset_1.dat"
-        output_name = "dataset_1211231231231231231.dat"
-        # TODO: input should not be in working directory
-        galaxy_input = os.path.join(galaxy_working, input_name)
-        with open(galaxy_input, "w") as handle:
-            handle.write("cow file contents\n")
-        galaxy_output = os.path.join(galaxy_working, output_name)
-        pulsar_input = os.path.join(self.staging_directory, job_id, "inputs", input_name)
-        pulsar_output = os.path.join(
-            self.staging_directory, job_id, "outputs", output_name
-        )
         with files_server("/", allow_multiple_downloads=True) as test_files_server:
-            files_endpoint = test_files_server.application_url
-            action = {
-                "name": "cow",
-                "type": "input",
-                "action": {
-                    "action_type": "json_transfer",
-                    "files_endpoint": files_endpoint,
-                    "path": galaxy_input,
-                },
-            }
-            client_inputs = [
-                ClientInput(
-                     path=galaxy_input,
-                     input_type="input_path",
-                ).action_source
-            ]
-            client_outputs = ClientOutputs(
-                working_directory=galaxy_working,
-                output_files=[os.path.join(galaxy_working, output_name)],
-            )
-            launch_params = dict(
-                command_line="cat '{}' > '{}'".format(galaxy_input, galaxy_output),
+            launch_params = self.setup_launch_params(
                 job_id=job_id,
-                setup_params=dict(
-                    job_id=job_id,
-                ),
-                setup=True,
-                remote_staging={
-                    "setup": [action],
-                    "action_mapper": self.setup_action_mapper(files_endpoint),
-                    "client_inputs": client_inputs,
-                    "client_outputs": client_outputs.to_dict(),
-                },
+                files_endpoint=test_files_server.application_url,
+                galaxy_working_directory=galaxy_working_directory,
+                pulsar_staging_directory=self.staging_directory,
+                input_directory=input_directory,
+                output_file_path=output_file_path,
             )
-            base64 = to_base64_json(launch_params)
-            assert not os.path.exists(galaxy_output)
-            submit.main(["--build_client_manager", "--base64", base64] + self._encode_application())
-            assert os.path.exists(galaxy_output)
-            out_contents = open(galaxy_output).read()
+            launch_params_base64 = to_base64_json(launch_params)
+
+            # submit job and test results
+            assert not os.path.exists(output_file_path)
+            submit.main(["--base64", launch_params_base64] + self.encode_application())
+            assert os.path.exists(output_file_path)
+            out_contents = open(output_file_path).read()
             assert out_contents == "cow file contents\n", out_contents
+
+    def setup_input_directory(self, directory):
+        pass
+
+    def setup_launch_params(
+            self,
+            *,
+            job_id,
+            files_endpoint,
+            galaxy_working_directory,
+            pulsar_staging_directory,
+            output_file_path,
+            **kwargs
+    ):
+        output_file_name = os.path.basename(output_file_path)
+        pulsar_output = os.path.join(pulsar_staging_directory, job_id, "outputs", output_file_name)
+        pulsar_input = os.path.join(pulsar_staging_directory, job_id, "inputs", "cow")
+        action = {
+            "name": "cow",
+            "type": "input",
+            "action": {
+                "action_type": "message",
+                "contents": "cow file contents\n"
+            }
+        }
+        client_outputs = ClientOutputs(
+            working_directory=galaxy_working_directory,
+            output_files=[output_file_path],
+        )
+        launch_params = dict(
+            command_line="cat '{}' > '{}'".format(pulsar_input, pulsar_output),
+            job_id=job_id,
+            setup_params=dict(
+                job_id=job_id,
+            ),
+            setup=True,
+            remote_staging={
+                "setup": [action],
+                "action_mapper": {
+                    "default_action": "remote_transfer",
+                    "files_endpoint": files_endpoint,
+                },
+                "client_outputs": client_outputs.to_dict(),
+            },
+        )
+        return launch_params
+
+    @abstractmethod
+    def encode_application(self):
+        pass
 
     @property
     def staging_directory(self):
-        return os.path.join(self.temp_directory, "staging")
+        return os.path.join(self.temp_directory, "pulsar_staging")
 
     @property
     def config_directory(self):
         config_directory = os.path.join(self.temp_directory, "config")
-        os.makedirs(config_directory)
+        os.makedirs(config_directory, exist_ok=True)
         return config_directory
 
 
@@ -96,7 +123,7 @@ class CliFileAppConfigTestCase(BaseCliTestCase):
     def test(self):
         self.run_and_check_submission()
 
-    def _encode_application(self):
+    def encode_application(self):
         app_conf = dict(
             staging_directory=self.staging_directory,
             message_queue_url="memory://submittest",
@@ -116,33 +143,10 @@ class CliCommandLineAppConfigTestCase(BaseCliTestCase):
     def test(self):
         self.run_and_check_submission()
 
-    def _encode_application(self):
+    def encode_application(self):
         app_conf = dict(
             staging_directory=self.staging_directory,
             message_queue_url="memory://submittest",
             conda_auto_init=False,
-        )
-        return ["--app_conf_base64", to_base64_json(app_conf)]
-
-
-class SequentialLocalCommandLineAppConfigTestCase(BaseCliTestCase):
-
-    @skip_unless_module("kombu")
-    @integration_test
-    def test(self):
-        self.run_and_check_submission()
-
-    def setup_action_mapper(self, files_endpoint):
-        return {
-            "default_action": "json_transfer",
-            "files_endpoint": files_endpoint,
-        }
-
-    def _encode_application(self):
-        app_conf = dict(
-            staging_directory=self.staging_directory,
-            message_queue_url="memory://submittest",
-            conda_auto_init=False,
-            manager={"type": "coexecution"}
         )
         return ["--app_conf_base64", to_base64_json(app_conf)]
