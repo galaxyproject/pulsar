3838f2c1ba74246079651f66b357d809910f15b6
nuwang <2070605+nuwang@users.noreply.github.com>
2025-05-07 19:24:34 +0530

    Add apptainer support

--- docker/kubernetes/entrypoint.sh
+++ docker/kubernetes/entrypoint.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 set -e
 
+# Configure docker host location
 export PATH=/usr/bin:$PATH
 export XDG_RUNTIME_DIR=/home/dockeruser/.docker/run
 export DOCKER_HOST=unix:///home/dockeruser/.docker/run/docker.sock
 
+# Configure container image data locations
 PULSAR_DATA_ROOT=/pulsar/files
 DOCKER_DATA_ROOT=$PULSAR_DATA_ROOT/docker
 DOCKER_LOG_FILE=$DOCKER_DATA_ROOT/docker-daemon.log
+APPTAINER_DATA_ROOT=$PULSAR_DATA_ROOT/apptainer
 
+# Setup directories
 mkdir -p $DOCKER_DATA_ROOT
+mkdir -p "$APPTAINER_DATA_ROOT"/{cache,tmp,localcache,library}
 chown pulsar:pulsar $PULSAR_DATA_ROOT
 chown dockeruser:dockeruser $DOCKER_DATA_ROOT
+chown -R pulsar:pulsar "$APPTAINER_DATA_ROOT"
+
+# Configure Apptainer (Singularity) environment variables
+export APPTAINER_CACHEDIR=$APPTAINER_DATA_ROOT/cache
+export APPTAINER_TMPDIR=$APPTAINER_DATA_ROOT/tmp
+export APPTAINER_LOCALCACHEDIR=$APPTAINER_DATA_ROOT/localcache
+export APPTAINER_LIBRARY=$APPTAINER_DATA_ROOT/library
 
 # Check if rootless is already set up
 if [ ! -d /home/dockeruser/.docker ]; then
